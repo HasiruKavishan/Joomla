1a20c8d084c6fabeb9aa2a47f19a1de03afeedb5
HasiruKavishan <bgh.kavishan@gmail.com>
2016-11-20 13:46:20 +0530

    joomla

---
+++ administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-18 05:41:36 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-18T05:41:36+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-20T06:07:23+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-20T06:07:40+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-20T06:18:35+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
